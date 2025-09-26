fe9ad995b4b4bfb56d5b835681eccfbd50660d8f
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2025-09-26 17:30:15 -0400

    Initial Commit

---
+++ Assignment-2/Assignment_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_2App.swift
+//  Assignment-2
+//
+//  Created by user278021 on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
